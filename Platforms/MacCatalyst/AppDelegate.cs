c6f08c73947852bd53c2dd7aebc1c553d02a06bd
Toyama <everton.toyama@gmail.com>
2024-10-09 10:11:46 -0300

    Projeto Inicial Criado

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace P.I_Noite_MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
